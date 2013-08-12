5d6dd57b7273673892d6a16deaebea3df26726c5
Radu Andries <admiral0@tuxfamily.org>
2013-08-12 16:29:33 +0200

    Added deployment files

---
+++ deploy/README.md
@@ -0,0 +1,10 @@
+# Contents of this directory
+ * DEPLOY.md - this file
+ * settings.py - Configuration file tweaked for production. You still need to edit it and replace the development one
+ in the TechnicAntani/TechnicAntani directory
+ * technicantani - init.d script for starting the web service
+ * technicantaninginx - nginx site configuration
+
+# Instructions
+
+Please refer to the wiki. https://github.com/AntaniCraft/TechnicAntani/wiki/Deploy
---
+++ deploy/settings.py
@@ -0,0 +1,121 @@
+#####################################################################################
+## Settings
+## Allsettings in this section MUST be edited for TechnicAntani to work
+#####################################################################################
+ADMINS = (
+    ('Your Name', 'your_email@example.com'),
+)
+# Make this unique, and don't share it with anybody.
+# on linux you can generate something random with `head /dev/urandom | tr -dc '[:graph:]' | head -c 64`
+SECRET_KEY = ''
+# Database configuration.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
+#####################################################################################
+## Situational Settings
+## Some of these settings may be required to be edited depending on your setup
+#####################################################################################
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = ''
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_URL = '/static/'
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = '/home/admiral0/Pubblici'
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'America/Chicago'
+
+
+#####################################################################################
+## Application Settings
+## This section doesn't need editing usually
+## Edit these only if you know what are you doing
+#####################################################################################
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = False
+USE_L10N = True
+USE_TZ = True
+STATICFILES_DIRS = ()
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+MANAGERS = ADMINS
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+ROOT_URLCONF = 'TechnicAntani.urls'
+WSGI_APPLICATION = 'TechnicAntani.wsgi.application'
+import os
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), '..', 'templates').replace('\\','/'),)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'manager',
+    'api',
+)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+INTERNAL_IPS = ('127.0.0.1',)
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
