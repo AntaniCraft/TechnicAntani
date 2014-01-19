4f66b417ab1f0c0a16de4c54cde3b7c36d14007f
Radu Andries <admiral0@tuxfamily.org>
2014-01-19 17:06:58 +0100

    Added initial cache app

--- TechnicAntani/urls.py
+++ TechnicAntani/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import patterns, include, url
 from TechnicAntani import settings
 
+import cachebuilder.views as cachebuilder
+
 urlpatterns = patterns('',
+                       # Auth stuffs
                        url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
                        url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+
+                       #Home - temporarily use cache builder
+                       url(r'^$', cachebuilder.index),
+
+                       #Cache builder
                        )
 
 if settings.DEBUG:
