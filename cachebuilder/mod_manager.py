822efa1cfdd0c363e7150ea5468c1ee8ab6e7648
Radu Andries <admiral0@tuxfamily.org>
2014-01-21 18:09:29 +0100

    Cleanup of unneeded methods

--- cachebuilder/mod_manager.py
+++ cachebuilder/mod_manager.py
@@ -19,9 +19,6 @@
 
 from os import path, walk, mkdir
 from shutil import move
-import tempfile
-import zipfile
-import json
 import hashlib
 from configparser import ConfigParser
 from TechnicAntani.settings import MODREPO_DIR
@@ -47,13 +44,6 @@ class Mod:
                 self.versions[pieces[1]].append(pieces[2].replace(".zip", ""))
 
 
-class ModType:
-    CONFIGPACK = 1
-    COREMOD = 2
-    NORMAL = 3
-    PREPACKAGED = 4
-
-
 class ModManager:
     def __init__(self):
         self.fspath = MODREPO_DIR
@@ -61,98 +51,6 @@ class ModManager:
         for root, dirs, files in walk(self.fspath):
             if "Metadata" in files:
                 self.mods.append(Mod(root, files))
-
-    def add_mod(self, slug, name, description, author, url, mcver, ver, tmpfile):
-        slugdir = self.fspath + path.sep + "mods"
-        if not path.exists(slugdir):
-            mkdir(slugdir)
-        slugdir += path.sep + slug
-        if not path.exists(slugdir):
-            mkdir(slugdir)
-        move(tmpfile,
-             self.fspath + path.sep + 'mods' + path.sep + slug + path.sep + slug + "-" + mcver + "-" + ver + ".zip"
-             )
-        c = ConfigParser()
-        c.add_section("Mod")
-        c.set("Mod", "Name", name)
-        c.set("Mod", "Description", description)
-        c.set("Mod", "Author", author)
-        c.set("Mod", "Url", url)
-        with open(slugdir + path.sep + "Metadata", "w") as fp:
-            c.write(fp)
-
-
-class RawMod:
-
-    def __init__(self, rawpath, modtype=ModType.NORMAL):
-        self.jarpath = rawpath
-        self.type = modtype
-        self.modid = ""
-        self.name = ""
-        self.desc = ""
-        self.ver = ""
-        self.authors = ""
-        self.url = ""
-        self.mcversion = ""
-
-    def read_metadata(self):
-        # TODO Rewrite this shit
-        z = zipfile.ZipFile(self.jarpath)
-        str = ""
-        try:
-            f = z.open("mcmod.info")
-            str = f.read()
-        except KeyError: #YOLO
-            return
-        obj = json.loads(str)
-        z.close()
-        try:
-            modinfo = obj[0]
-            self.modid = modinfo["modid"]
-        except Exception: #YOLO
-            pass
-        try:
-            self.name = modinfo["name"]
-        except Exception: #YOLO
-            pass
-        try:
-            self.desc = modinfo["description"]
-        except Exception: #YOLO
-            pass
-        try:
-            self.ver = modinfo["version"]
-        except Exception: #YOLO
-            pass
-        try:
-            self.url = modinfo["url"]
-        except Exception: #YOLO
-            pass
-        try:
-            self.authors = ",".join(modinfo["authors"])
-        except Exception: #YOLO
-            pass
-        try:
-            self.mcversion = modinfo["mcversion"]
-        except Exception: #YOLO
-            pass
-
-    def pack(self, fname):
-        tmpfile = tempfile.mktemp(suffix=".zip")
-        if self.type == ModType.PREPACKAGED:
-            return self.jarpath
-        zpack = zipfile.ZipFile(tmpfile, "w")
-        name = fname + ".jar"
-        zipname = ""
-        if self.type == ModType.NORMAL:
-            zipname = "mods/" + name
-        elif self.type == ModType.CONFIGPACK:
-            zipname = "config/" + name
-        else:
-            zipname = "coremods/" + name
-
-        zpack.write(self.jarpath, zipname)
-        zpack.close()
-        return tmpfile
 
 
 def checksum_file(dirpath):
